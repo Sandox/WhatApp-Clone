4ec2a3a737eb995601464b55f0d3f4126404601d
Pman143 <sandoxngwenya@gmail.com>
2021-07-15 19:36:31 +0200

    background- styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
+import { ChatBackgroundComponent } from './chat-background/chat-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    ChatBackgroundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat-background/chat-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-background',
+  templateUrl: './chat-background.component.html',
+  styleUrls: ['./chat-background.component.css']
+})
+export class ChatBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
